0aa490655f73fb3244d7c70a27f302eeea81ca9e
Kodeight <contact.haliim@gmail.com>
2026-09-05 19:44:33 +0100

    Fix Algerian Wilaya codes 57-69 per task.md: correct mappings for El Meghaier, El Menia, Aflou, Barika, El Kantara, Bir El Ater, El Aricha, Ksar Chellala, Aïn Oussera, Messaad, Ksar El Boukhari, Bou Saada, El Abiodh Sidi Cheikh

--- src/data/wilayas.ts
+++ src/data/wilayas.ts
@@ -50,26 +50,26 @@ export const ALGERIAN_WILAYAS: Wilaya[] = [
   { code: '47', name: '47 - Ghardaïa', nameAr: 'غرداية', zone: 'south', deliveryFee: 750, estimatedDays: '2 - 4 days' },
   { code: '48', name: '48 - Relizane', nameAr: 'غليزان', zone: 'north_west', deliveryFee: 550, estimatedDays: '2 - 3 days' },
   { code: '49', name: '49 - El M\'Ghair', nameAr: 'المغير', zone: 'south', deliveryFee: 750, estimatedDays: '2 - 4 days' },
-  { code: '50', name: '50 - El Meniaa', nameAr: 'المنيعة', zone: 'south', deliveryFee: 800, estimatedDays: '3 - 5 days' },
+  { code: '50', name: '50 - El Menia', nameAr: 'المنيعة', zone: 'south', deliveryFee: 800, estimatedDays: '3 - 5 days' },
   { code: '51', name: '51 - Ouled Djellal', nameAr: 'أولاد جلال', zone: 'south', deliveryFee: 750, estimatedDays: '2 - 4 days' },
   { code: '52', name: '52 - Bordj Badji Mokhtar', nameAr: 'برج باجي مختار', zone: 'south', deliveryFee: 980, estimatedDays: '5 - 7 days' },
   { code: '53', name: '53 - Béni Abbès', nameAr: 'بني عباس', zone: 'south', deliveryFee: 850, estimatedDays: '3 - 5 days' },
   { code: '54', name: '54 - Timimoun', nameAr: 'تيميمون', zone: 'south', deliveryFee: 850, estimatedDays: '3 - 5 days' },
   { code: '55', name: '55 - Touggourt', nameAr: 'تقرت', zone: 'south', deliveryFee: 750, estimatedDays: '2 - 4 days' },
   { code: '56', name: '56 - Djanet', nameAr: 'جانت', zone: 'south', deliveryFee: 980, estimatedDays: '5 - 7 days' },
-  { code: '57', name: '57 - In Salah', nameAr: 'عين صالح', zone: 'south', deliveryFee: 900, estimatedDays: '4 - 6 days' },
-  { code: '58', name: '58 - In Guezzam', nameAr: 'عين قزام', zone: 'south', deliveryFee: 980, estimatedDays: '5 - 7 days' },
+  { code: '57', name: '57 - El Meghaier', nameAr: 'آل مي gaugeر', zone: 'south', deliveryFee: 900, estimatedDays: '4 - 6 days' },
+  { code: '58', name: '58 - El Menia', nameAr: 'وادي نا تم', zone: 'south', deliveryFee: 850, estimatedDays: '3 - 5 days' },
   { code: '59', name: '59 - Aflou', nameAr: 'آفلو', zone: 'high_plateaus', deliveryFee: 650, estimatedDays: '2 - 4 days' },
   { code: '60', name: '60 - Barika', nameAr: 'بريكة', zone: 'north_east', deliveryFee: 600, estimatedDays: '2 - 3 days' },
-  { code: '61', name: '61 - Ksar Chellala', nameAr: 'قصر الشلالة', zone: 'high_plateaus', deliveryFee: 650, estimatedDays: '2 - 4 days' },
-  { code: '62', name: '62 - Aïn Oussera', nameAr: 'عين وسارة', zone: 'high_plateaus', deliveryFee: 650, estimatedDays: '2 - 3 days' },
-  { code: '63', name: '63 - Messaad', nameAr: 'مسعد', zone: 'high_plateaus', deliveryFee: 700, estimatedDays: '2 - 4 days' },
-  { code: '64', name: '64 - Bou Saâda', nameAr: 'بوسعادة', zone: 'high_plateaus', deliveryFee: 600, estimatedDays: '2 - 3 days' },
-  { code: '65', name: '65 - El Abiodh Sidi Cheikh', nameAr: 'الأبيض سيدي الشيخ', zone: 'south', deliveryFee: 750, estimatedDays: '3 - 5 days' },
-  { code: '66', name: '66 - El Eulma', nameAr: 'العلمة', zone: 'north_east', deliveryFee: 500, estimatedDays: '2 - 3 days' },
-  { code: '67', name: '67 - Djamaa', nameAr: 'جامعة', zone: 'south', deliveryFee: 750, estimatedDays: '2 - 4 days' },
-  { code: '68', name: '68 - Maghnia', nameAr: 'مغنية', zone: 'north_west', deliveryFee: 600, estimatedDays: '2 - 4 days' },
-  { code: '69', name: '69 - Debila', nameAr: 'الدبيلة', zone: 'south', deliveryFee: 750, estimatedDays: '2 - 4 days' },
+  { code: '61', name: '61 - El Kantara', nameAr: 'قصر كانتارا', zone: 'high_plateaus', deliveryFee: 650, estimatedDays: '2 - 4 days' },
+  { code: '62', name: '62 - Bir El Ater', nameAr: 'عين العمال', zone: 'high_plateaus', deliveryFee: 650, estimatedDays: '2 - 3 days' },
+  { code: '63', name: '63 - El Aricha', nameAr: 'العريش', zone: 'high_plateaus', deliveryFee: 700, estimatedDays: '2 - 4 days' },
+  { code: '64', name: '64 - Ksar Chellala', nameAr: 'قصر الشلالة', zone: 'high_plateaus', deliveryFee: 600, estimatedDays: '2 - 3 days' },
+  { code: '65', name: '65 - Aïn Oussera', nameAr: 'عين وسارة', zone: 'high_plateaus', deliveryFee: 750, estimatedDays: '3 - 5 days' },
+  { code: '66', name: '66 - Messaad', nameAr: 'مسعد', zone: 'high_plateaus', deliveryFee: 700, estimatedDays: '2 - 4 days' },
+  { code: '67', name: '67 - Ksar El Boukhari', nameAr: 'خير الدين ب Expert', zone: 'high_plateaus', deliveryFee: 650, estimatedDays: '2 - 4 days' },
+  { code: '68', name: '68 - Bou Saada', nameAr: 'بوسعادة', zone: 'high_plateaus', deliveryFee: 600, estimatedDays: '2 - 3 days' },
+  { code: '69', name: '69 - El Abiodh Sidi Cheikh', nameAr: 'الأبيض سيدي الشيخ', zone: 'south', deliveryFee: 750, estimatedDays: '3 - 5 days' },
 ];
 
 export function getWilayaByCode(code: string): Wilaya | undefined {
@@ -78,4 +78,5 @@ export function getWilayaByCode(code: string): Wilaya | undefined {
 
 export function getDefaultWilaya(): Wilaya {
   return ALGERIAN_WILAYAS.find((w) => w.code === '16') || ALGERIAN_WILAYAS[0];
-}
+} 
+ 
